4a34bf7771a75f3eca72b810f6b236578e95fe97
YanUsenko <63456237+YanUsenko@users.noreply.github.com>
2021-01-03 21:16:32 +0100

    Add files via upload
    debug v1

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
-const menuBtn = document.querySelector(".mobile-menu-button");
-const menu = document.querySelector(".mobile-menu_nav");
-const menuLine = document.querySelector(".focus_line");
+const menuBtn = document.querySelector(".header__menu-btn");
+const menu = document.querySelector(".header__mobile-nav");
+const menuLine = document.querySelector(".active-link");
+const body = document.querySelector("body");
+const menuLink = document.querySelector(".header__mobile-link");
+const menuLink2 = document.querySelector(".header__mobile-link-2");
+const menuLink3 = document.querySelector(".header__mobile-link-3");
 
 menuBtn.addEventListener("click", function() {
     menu.classList.toggle("active");
+    menuLine.classList.toggle("line-focus");
+    body.classList.toggle("hidden");
 });
 
-menuBtn.addEventListener("click", function() {
+menuLink.addEventListener("click", function() {
+    menu.classList.toggle("active");
     menuLine.classList.toggle("line-focus");
+    
+});
+menuLink2.addEventListener("click", function() {
+    menu.classList.toggle("active");
+    menuLine.classList.toggle("line-focus");
+    
+});
+menuLink3.addEventListener("click", function() {
+    menu.classList.toggle("active");
+    menuLine.classList.toggle("line-focus");
+    
 });
 
 document.querySelector(".mobile-menu-button").onclick = function(){
     this.classList.toggle("focus");
 }
-
-$(document).ready(function(){
-    $('.slider').slick({
-        adaptiveHeight: true,
-        infinite: true,
-        slidesToShow: 1,
-        variableWidth: true,
-        prevArrow: '.arrows-prev',
-        nextArrow: '.arrows-next',
-        dots: true,
-        appendDots: '.dots',
-        autoplay: true,
-        autoplaySpeed: 4000,
-        speed: 1000,
-    });
-});
